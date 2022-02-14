b0375f8070365b0f61c19eed9a3cc2f1ff985e7a
Ashik David Roy <84988968+AshikDavidRoy@users.noreply.github.com>
2022-02-14 08:30:31 +0530

    Update BubbleSort.c

--- Ashik-S3-B/BubbleSort.c
+++ Ashik-S3-B/BubbleSort.c
@@ -3,21 +3,22 @@ int main()
 {
 	int ar[50],n,c,e,swp,cut=0;
 	printf("Enter The No. Element of the Array : ");
+	cut++;
 	scanf("%d",&n);
-	cut=cut++;
+	cut++;
 	for(c=0;c<n;c++)
-	{
+	{cut++;
 		scanf("%d",&ar[c]);
 		cut=cut++;
 	}
 	cut=cut++;
 	
 	for(c=0;c<n-1;c++)
-	{
+	{cut++;
 		for (e=0;e<n-c-1;e++)
-		{
+		{cut++;
 			if(ar[e]>ar[e+1])
-			{
+			{cut++;
 				swp = ar[e];
 				cut=cut++;
 				ar[e]=ar[e+1];
@@ -33,12 +34,17 @@ int main()
 	cut=cut++;
 	
 	printf("\n Array is Sorted :\n");
+	cut++;
 	for (c=0;c<n;c++)
 
-	{	
+	{	cut++;
 		printf("%d \n",ar[c]);
+	 cut++;
 	}
+	cut++;
+	cut++;
+	cut++;
 	printf("\n%d\n",cut);
-	printf("\nSpace 	Complexity : 240\n");
+	printf("\nSpace Complexity : 240\n");
 	return 0;
 }
